eabb69d8cc5af9371226835d01e974fb46db7a7e
Jhonatan Ferrer <111335841+JhonatanFerrer@users.noreply.github.com>
2024-05-10 19:14:56 -0500

    Update theme.lua

--- awesome/theme/theme.lua
+++ awesome/theme/theme.lua
@@ -102,8 +102,7 @@ theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/m
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
 theme.wallpaper = "~/.config/awesome/theme/wallpaper2.jpg"
---theme.wallpaper = "~/.config/awesome/theme/wallpaper2.png"
-
+-- The wallpaper isn't mine, I found it in DevianArt. Here is the link https://www.deviantart.com/waifuxart/art/Wallpaper-Series-1-968741815
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
